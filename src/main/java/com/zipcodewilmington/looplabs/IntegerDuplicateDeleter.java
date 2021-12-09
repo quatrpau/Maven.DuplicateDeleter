43046b44e9875628f8035d1d0fcfdca22cf1ce9c
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-12-09 15:11:02 -0500

    finished lab

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,17 +1,53 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
-        return new Integer[0];
+        if(maxNumberOfDuplications == 0){
+            return new Integer[0];
+        }
+        Integer[] returnal = array;
+        for(Integer str: returnal){
+            long duplicates;
+            if((duplicates = Arrays
+                    .stream(returnal)
+                    .filter(q->q.equals(str))
+                    .count()) >= maxNumberOfDuplications){
+                Integer[] temper = returnal;
+                long lamdup = duplicates;
+                returnal = Arrays
+                        .stream(temper)
+                        .filter(q->!q.equals(str))
+                        .toArray(q-> Arrays.copyOf(temper, temper.length - (int) lamdup));
+            }
+        }
+        return returnal;
     }
 
     @Override
     public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new Integer[0];
+        Integer[] returnal = array;
+        for(Integer inta: returnal){
+            if(Arrays.stream(returnal)
+                    .filter(q->q.equals(inta))
+                    .count() == exactNumberOfDuplications){
+                Integer[] temper = returnal;
+                returnal = Arrays
+                        .stream(temper)
+                        .filter(q->!q.equals(inta))
+                        .toArray(q-> Arrays.copyOf(temper, temper.length - exactNumberOfDuplications));
+            }
+        }
+        return returnal;
     }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,24 +1,59 @@
 package com.zipcodewilmington.looplabs;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+import java.util.stream.Stream;
 
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        if(maxNumberOfDuplications == 0){
+            return new String[0];
+        }
+        String[] returnal = array;
+        for(String str: returnal){
+            long duplicates;
+            if((duplicates = Arrays
+                    .stream(returnal)
+                    .filter(q->q.equals(str))
+                    .count()) >= maxNumberOfDuplications){
+                String[] temper = returnal;
+                long lamdup = duplicates;
+                returnal = Arrays
+                        .stream(temper)
+                        .filter(q->!q.equals(str))
+                        .toArray(q-> Arrays.copyOf(temper, temper.length - (int) lamdup));
+            }
+        }
+        return returnal;
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        if(exactNumberOfDuplications == 0){
+            return new String[0];
+        }
         String[] returnal = array;
-        for(String str: array){
-            if(Arrays.stream(array).filter(q->q.equals(str)).count() == exactNumberOfDuplications){
-                returnal = Arrays.stream(returnal).filter(q->!q.equals(str)).toArray(q-> Arrays.copyOf(returnal, returnal.length - exactNumberOfDuplications));
+        for(String str: returnal){
+            if(Arrays
+                    .stream(returnal)
+                    .filter(q->q.equals(str))
+                    .count() == exactNumberOfDuplications){
+                String[] temper = returnal;
+                returnal = Arrays
+                        .stream(temper)
+                        .filter(q->!q.equals(str))
+                        .toArray(q-> Arrays.copyOf(temper, temper.length - exactNumberOfDuplications));
             }
         }
+        return returnal;
     }
 }
